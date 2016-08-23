2d955ac413565da8e74b994d13a4068483b8b0f3
Troy Hunt <troyhunt@hotmail.com>
2016-08-23 11:40:35 +1000

    GA code goes to a different place in source to rest of the JS so it gets separate files

--- assets/js/troyhunt.js
+++ assets/js/troyhunt.js
@@ -18,12 +18,3 @@ var disqus_shortname = 'troyhunt';
     s.src = '//' + disqus_shortname + '.disqus.com/count.js';
     (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
 }());
-
-// Google Analytics
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-10949399-1', 'auto');
-ga('send', 'pageview');
