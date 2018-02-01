bf582b78f6907475c41ae5f82ffe980cbbfce0ea
Troy Hunt <troyhunt@hotmail.com>
2018-02-01 17:33:43 +1000

    Replaced social onclick with listners to avoid CSP violations

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,33 @@
-! function() {
-    function init() {
-        initEvents()
-    }
+! function () {
+  function init() {
+    initEvents()
+  }
 
-    function initEvents() {
-        openbtn.addEventListener("click", toggleMenu), closebtn && closebtn.addEventListener("click", toggleMenu), content.addEventListener("click", function(ev) {
-            var target = ev.target;
-            isOpen && target !== openbtn && toggleMenu()
-        })
-    }
+  function initEvents() {
+    openbtn.addEventListener("click", toggleMenu), closebtn && closebtn.addEventListener("click", toggleMenu), content.addEventListener("click", function (ev) {
+      var target = ev.target;
+      isOpen && target !== openbtn && toggleMenu()
+    })
+  }
 
-    function toggleMenu() {
-        isOpen ? classie.remove(bodyEl, "show-menu") : classie.add(bodyEl, "show-menu"), isOpen = !isOpen
-    }
-    var bodyEl = document.body,
-        content = document.querySelector("#container"),
-        openbtn = document.getElementById("open-button"),
-        closebtn = document.getElementById("close-button"),
-        isOpen = !1;
-    init()
+  function toggleMenu() {
+    isOpen ? classie.remove(bodyEl, "show-menu") : classie.add(bodyEl, "show-menu"), isOpen = !isOpen
+  }
+  var bodyEl = document.body,
+    content = document.querySelector("#container"),
+    openbtn = document.getElementById("open-button"),
+    closebtn = document.getElementById("close-button"),
+    isOpen = !1;
+  init()
+
+  var openSocialWindow = function (evt) {
+    evt.preventDefault();
+    floating_subscribe.style.display = 'none';
+    window.open(this.href, 'social-share', 'width=580,height=296');
+  };
+
+  var socialIcons = document.getElementsByClassName("article-open_window");
+  for (var i = 0; i < socialIcons.length; i++) {
+    socialIcons[i].addEventListener('click', openSocialWindow, false);
+  }
 }();
