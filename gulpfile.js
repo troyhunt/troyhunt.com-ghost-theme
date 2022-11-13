9da40427ee668cb812c601c4e0e25477ee0f18b9
Troy Hunt <github@troyhunt.com>
2022-11-14 09:35:59 +1000

    Fixed indentation

--- gulpfile.js
+++ gulpfile.js
@@ -8,28 +8,28 @@ var gulp = require('gulp'),
 gulp.task('bundle-minify-js', function () {
   return gulp.src(['assets/js/classie.js', 'assets/js/main.js', 'assets/js/blog-data.js', 'assets/js/disqus.js', 'assets/js/subscribe.js'])
     .pipe(uglify())
-	.pipe(concat('app.js'))
+	  .pipe(concat('app.js'))
     .pipe(gulp.dest('assets/js'))
 });
 
 gulp.task('bundle-minify-google-analytics', function () {
   return gulp.src(['assets/js/google-analytics.js'])
     .pipe(uglify())
-	.pipe(concat('google-analytics.min.js'))
+	  .pipe(concat('google-analytics.min.js'))
     .pipe(gulp.dest('assets/js'))
 });
 
 gulp.task('bundle-minify-events-js', function () {
   return gulp.src(['assets/js/events.js'])
     .pipe(uglify())
-	.pipe(concat('events.min.js'))
+	  .pipe(concat('events.min.js'))
     .pipe(gulp.dest('assets/js'))
 });
 
 gulp.task('styles-build', function() {
   return gulp.src('assets/css/main.css')
     .pipe(mmq())
-	.pipe(cssmin())
+	  .pipe(cssmin())
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('assets/css'))
 });
